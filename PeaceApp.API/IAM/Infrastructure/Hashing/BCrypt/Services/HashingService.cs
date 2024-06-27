483fc069eb12753c22fcce98bd4cc9fe3773a444
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-06-27 00:17:12 -0500

    feat(iam): hashing service in infrastructure layer

---
+++ PeaceApp.API/IAM/Infrastructure/Hashing/BCrypt/Services/HashingService.cs
@@ -0,0 +1,16 @@
+using PeaceApp.API.IAM.Application.Internal.OutboundServices;
+using BCryptNet = BCrypt.Net.BCrypt;
+namespace PeaceApp.API.IAM.Infrastructure.Hashing.BCrypt.Services;
+
+public class HashingService : IHashingService
+{
+    public string HashPassword(string password)
+    {
+        return BCryptNet.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password, string passwordHash)
+    {
+        return BCryptNet.Verify(password, passwordHash);
+    }
+}
