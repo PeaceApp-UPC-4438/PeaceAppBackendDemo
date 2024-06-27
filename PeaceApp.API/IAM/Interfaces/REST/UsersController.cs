4799f4caac66f858b4aaf5eb96900abd1b19bd3b
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-06-27 03:39:43 -0500

    feat(iam): iam controllers

---
+++ PeaceApp.API/IAM/Interfaces/REST/UsersController.cs
@@ -0,0 +1,36 @@
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using PeaceApp.API.IAM.Domain.Model.Queries;
+using PeaceApp.API.IAM.Domain.Services;
+using PeaceApp.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using PeaceApp.API.IAM.Interfaces.REST.Transform;
+
+namespace PeaceApp.API.IAM.Interfaces.REST;
+
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class UsersController(IUserQueryService userQueryService) : ControllerBase
+{
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var getUserByIdQuery = new GetUserByIdQuery(id);
+        var user = await userQueryService.Handle(getUserByIdQuery);
+        if (user is null) return NotFound();
+        var userResource = UserResourceFromEntityAssembler.ToResourceFromEntity(user);
+        return Ok(userResource);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var getAllUsersQuery = new GetAllUsersQuery();
+        var users = await userQueryService.Handle(getAllUsersQuery);
+        var userResources = users.Select(UserResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(userResources);
+    }
+}
